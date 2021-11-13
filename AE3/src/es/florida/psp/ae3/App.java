6f898456a9715b81288e02e5ea153304430942a9
Eidursson <borealcode.edv@gmail.com>
2021-11-13 13:27:51 +0100

    revisar ampliacion

--- AE3/src/es/florida/psp/ae3/App.java
+++ AE3/src/es/florida/psp/ae3/App.java
@@ -3,8 +3,6 @@ package es.florida.psp.ae3;
 import java.util.ArrayList;
 
 public class App {
-	
-	
 	public static void main(String[] args) {
 		ArrayList<Minero> mineros = new ArrayList<Minero>();
 		Mina mina = new Mina(100);
@@ -16,10 +14,34 @@ public class App {
 			t = new Thread(minero);
 			t.setName("Minero "+(i+1));
 			t.start();
-			//System.out.println(t.getName()+" entra a la mina");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		} // end-for
+		
+		Ventilador ventilador = new Ventilador(false);
+		// Hilo encenderVentilador
+		Thread hiloEncender = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				ventilador.encenderVentilador();
+			} // end-run
+		}); // end-thread
+		// Hilo apagarVentilador
+		Thread hiloApagar = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				ventilador.apagarVentilador();
+			} // end-run
+		}); // end-thread
+		hiloEncender.start();
+		hiloApagar.start();		
+		
 		try {
-			Thread.sleep(5000); // en ms
+			Thread.sleep(12500); // en ms
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- AE3/src/es/florida/psp/ae3/Ventilador.java
+++ AE3/src/es/florida/psp/ae3/Ventilador.java
@@ -2,61 +2,44 @@ package es.florida.psp.ae3;
 
 public class Ventilador {
 
-	int estadoVentilador = 1;
+	boolean modoFuncionamiento = false; // true = encendido | false = apagado
 	int tiempoFuncionamiento = 5000;
+	
+	public Ventilador (boolean modoFuncionamiento) {
+		this.modoFuncionamiento = modoFuncionamiento;
+	}
 
-	public void encenderSemaforo1() {
+	public void encenderVentilador() {
 		while(true) {
 			synchronized (this) {
 				try {
-					while (estadoVentilador == 2) wait();
-					System.err.print("Semaforo 2 rojo");
-					System.out.println(" -> Semaforo 1 verde durante "+tiempoFuncionamiento/1000+" segundos");
-					Thread.sleep(tiempoFuncionamiento);
-					estadoVentilador = 2;
+					while (modoFuncionamiento == false) wait();
+					System.err.print("ESTADO: ");
+					System.out.println("--> Ventilador encendido durante "+tiempoFuncionamiento/1000+" segundos");
+					modoFuncionamiento = false;
 					notify();
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				} // end-try-catch
 			} // end-synchronized
 		} // end-while
-	} // end-encenderSemaforo1
+	} // end-encenderVentilador
 
-	public void encenderSemaforo2() {
+	public void apagarVentilador() {
 		while (true) {
 			synchronized (this) {
 				try {
-					while (estadoVentilador == 1) wait();
-					System.err.print("Semaforo 1 rojo");
-					System.out.println(" -> Semaforo 2 verde durante "+tiempoFuncionamiento/1000+" segundos");
+					while (modoFuncionamiento == true) wait();
+					System.err.print("ESTADO: ");
+					System.out.println("--> Ventilador apagado durante "+tiempoFuncionamiento/1000+" segundos");
 					Thread.sleep(tiempoFuncionamiento);
-					estadoVentilador = 1;
+					modoFuncionamiento = true;
 					notify();
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				} // end-try-catch
 			} // end-synchronized
 		} // end-while
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Ventilador ventilador = new Ventilador();
-		// Hilo semáforo 1 encendido
-		Thread t1 = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				ventilador.encenderSemaforo1();
-			}
-		});
-		// Hilo semáforo 2 encendido
-		Thread t2 = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				ventilador.encenderSemaforo2();
-			}
-		});
-		t1.start();
-		t2.start();
-	} // end-main
+	} // end-apagarVentilador
 } // end-class
 
