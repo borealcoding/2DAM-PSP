89a8fbb6ef318a850b9d6ad54ac02021592a9c72
Eidursson <borealcode.edv@gmail.com>
2021-11-24 20:16:54 +0100

    empiezo proyectos a4 y ae4

---
+++ A4/src/es/florida/psp/a4/GestorDescargas.java
@@ -0,0 +1,10 @@
+package es.florida.psp.a4;
+
+public class GestorDescargas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ A4/src/es/florida/psp/a4/GestorDescargasMejorado.java
@@ -0,0 +1,10 @@
+package es.florida.psp.a4;
+
+public class GestorDescargasMejorado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
